26569b2be82abca7405b0534941750784d2c0582
Sam Der <samder2468@gmail.com>
2020-06-22 22:14:15 -0700

    Updates

---
+++ precache-manifest.9a933594ae86c1835ef3cba893c49dcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c2f7037f2a115d15ed4e726fb716c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f384e39a040e0e65fcca",
+    "url": "/static/css/main.129abb8c.chunk.css"
+  },
+  {
+    "revision": "24444de94f45909b42e5",
+    "url": "/static/js/2.46d23136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.46d23136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f384e39a040e0e65fcca",
+    "url": "/static/js/main.ab3160ae.chunk.js"
+  },
+  {
+    "revision": "a6c9a5364fcff26f5c2f",
+    "url": "/static/js/runtime-main.433b0a83.js"
+  }
+]);

2bcc9bd5423619caf71a94bd7749d34610b7fe36
Sam Der <samder2468@gmail.com>
2020-06-24 22:52:41 -0700

    Updates

---
+++ precache-manifest.8d03271363f2a2e5e8110a2e7abb0957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e32807070618e3bb6ee7d6e214f0ae",
+    "url": "/mathriddles/index.html"
+  },
+  {
+    "revision": "9102b29fc7d49ee8a756",
+    "url": "/mathriddles/static/css/main.a651fb58.chunk.css"
+  },
+  {
+    "revision": "3f1772172b7dc6f5d796",
+    "url": "/mathriddles/static/js/2.2d60e6dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mathriddles/static/js/2.2d60e6dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9102b29fc7d49ee8a756",
+    "url": "/mathriddles/static/js/main.00551e25.chunk.js"
+  },
+  {
+    "revision": "87be166b6ce2d7071eb3",
+    "url": "/mathriddles/static/js/runtime-main.7a697b4c.js"
+  }
+]);

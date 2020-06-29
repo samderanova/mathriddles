fdfc99454015131fa2aec93532cbdb09e1838243
Sam Der <samder2468@gmail.com>
2020-06-29 13:03:31 -0700

    fixing display flex on iOS

--- build/precache-manifest.f33bca1faa1f1ae29c7bce9996cb18e6.js
+++ build/precache-manifest.f33bca1faa1f1ae29c7bce9996cb18e6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "afc7da388e93c647d2d178574843f252",
+    "revision": "f941ed7e6106337febcf908ffd283cd0",
     "url": "/mathriddles/index.html"
   },
   {
-    "revision": "7143a47ff0686285e675",
-    "url": "/mathriddles/static/css/main.1995be92.chunk.css"
+    "revision": "da2482bb249ce57463b7",
+    "url": "/mathriddles/static/css/main.7ac8d6d6.chunk.css"
   },
   {
     "revision": "2befabc081a253e56b2e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mathriddles/static/js/2.d6959bdf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7143a47ff0686285e675",
-    "url": "/mathriddles/static/js/main.b7dd9e57.chunk.js"
+    "revision": "da2482bb249ce57463b7",
+    "url": "/mathriddles/static/js/main.17c635ff.chunk.js"
   },
   {
     "revision": "87be166b6ce2d7071eb3",
